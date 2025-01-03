f8427868c2d841cb8e5e6deb283d78929697208b
Pouyan Heyratpour <me@pouyan.dev>
2025-01-03 04:07:09 +0330

    feat(rebel)/schema: speed control over i2c
    Signed-off-by: Pouyan Heyratpour <me@pouyan.dev>

---
+++ rebel/README.md
@@ -0,0 +1,5 @@
+# Build
+
+```shell
+cmake -DCMAKE_TOOLCHAIN_FILE=avr-gcc-toolchain.cmake
+```
---
+++ rebel/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+
+int main() {
+	return 0;
+}
