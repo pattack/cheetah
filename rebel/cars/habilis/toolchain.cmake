b11673163d9f58134713b16407be5bc2b3db075d
Pouyan Heyratpour <me@pouyan.dev>
2026-03-08 13:42:25 +0330

    chore(rebel): moved hal compile into hal cmake spec
    Signed-off-by: Pouyan Heyratpour <me@pouyan.dev>

--- rebel/cars/habilis/toolchain.cmake
+++ rebel/cars/habilis/toolchain.cmake
@@ -19,13 +19,15 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 # Target CPU options
 set(TARGET_CPU_FLAGS "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16")
+set(TARGET_COMPILE_FLAGS "-ffunction-sections -fdata-sections")
 
 # No standard libraries
-set(CMAKE_C_FLAGS_INIT "${TARGET_CPU_FLAGS} -ffreestanding")
-set(CMAKE_CXX_FLAGS_INIT "${TARGET_CPU_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
+set(CMAKE_C_FLAGS_INIT "${TARGET_CPU_FLAGS} ${TARGET_COMPILE_FLAGS} -ffreestanding")
+set(CMAKE_CXX_FLAGS_INIT "${TARGET_CPU_FLAGS} ${TARGET_COMPILE_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
 set(CMAKE_ASM_FLAGS_INIT "${TARGET_CPU_FLAGS} -x assembler-with-cpp -MMD -MP")
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "${TARGET_CPU_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT} --specs=nano.specs")
 set(CMAKE_EXE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT} -Wl,-Map=${CMAKE_PROJECT_NAME}.map")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT} -Wl,--gc-sections")
 set(CMAKE_EXE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT} -Wl,--print-memory-usage")
