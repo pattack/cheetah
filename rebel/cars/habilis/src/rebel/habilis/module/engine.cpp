ea5bafa9895c07cf9c37765dccf7aa2af4356a96
Pouyan Heyratpour <me@pouyan.dev>
2026-03-28 23:43:00 +0330

    chore(rebel): remove unused habilis codes
    Signed-off-by: Pouyan Heyratpour <me@pouyan.dev>

--- rebel/cars/habilis/src/rebel/habilis/module/engine.cpp
+++ rebel/cars/habilis/src/rebel/habilis/module/engine.cpp
@@ -3,8 +3,6 @@
 //
 
 #include <rebel/habilis/module/engine.hpp>
-#include <rebel/habilis/module/engine_loopback.hpp>
-#include <rebel/habilis/module/engine_loopless.hpp>
 
 namespace Rebel::Habilis::Module {
     Engine::Engine(const Rebel::Habilis::Component::PCA9685 &driver) : driver(driver) {
